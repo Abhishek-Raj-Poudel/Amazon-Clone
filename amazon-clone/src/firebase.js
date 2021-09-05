fa92a7575e2bfa0da25023135b24db9caa231f0a
Abhishek-Raj-Poudel <89299685+Abhishek-Raj-Poudel@users.noreply.github.com>
2021-09-06 00:23:20 +0545

    Made Navbar
    Made Navbar just css

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyADrWtIIe2iqlqcr3ZVWxWn19NQNyR0UtI",
+  authDomain: "clone-aff01.firebaseapp.com",
+  projectId: "clone-aff01",
+  storageBucket: "clone-aff01.appspot.com",
+  messagingSenderId: "520599609170",
+  appId: "1:520599609170:web:440f211fe64845ba37a48b",
+  measurementId: "G-2QD8MR9FJW"
+};
